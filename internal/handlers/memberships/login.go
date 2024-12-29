cbc708c221be3cb699192fa2fc0370c6c3a02c04
jetaimejeteveux <akufirman22@gmail.com>
2024-12-29 21:42:08 +0700

    feat: add user login

---
+++ internal/handlers/memberships/login.go
@@ -0,0 +1,27 @@
+package memberships
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jetaimejeteveux/music-catalog/internal/models/memberships"
+	"net/http"
+)
+
+func (h *Handler) Login(c *gin.Context) {
+	var req memberships.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	accessToken, err := h.service.Login(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, memberships.LoginResponse{
+		AccessToken: accessToken,
+	})
+}
